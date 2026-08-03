708ea6288ce8c4c546aa9a11aaa5d9f0658fa0c6
harshpratik2411 <harshsrivastava2357@gmail.com>
2026-08-04 04:50:39 +0530

    databse connected sucessfully

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,19 @@
-import mongoose from "mongoose"; 
-import {DB_NAME} from "./constants.js";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
 
-const connectDB = async () =>{
-     try
-     {
-       const connectionInstance =  await mongoose.connect
-       (`${process.env.DB_NAME}/${DB_NAME}`)
-         console.log(`/n mongoDB  connected!! DB HOST : ${connectionInstance.connection.host}` )
-        
-     } catch (error){
-         console.log('Error connecting database', error);
-        // throw error;
-        process.exit(1);
-     }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+
+    console.log(
+      `MongoDB connected!! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Error connecting database", error);
+    process.exit(1);
+  }
+};
+
 export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants.js"; 
 
 
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
+dotenv.config({
+ path: "./.env"
+});
+connectDB();
 
 
 
