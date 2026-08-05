9c82b70c554ed1236c7418f12d69b15b00e8ca44
harshpratik2411 <harshsrivastava2357@gmail.com>
2026-08-05 14:30:39 +0530

    cors and cookie parser installed

--- src/index.js
+++ src/index.js
@@ -5,8 +5,17 @@ import connectDB from "./db/index.js";
 
 dotenv.config({
  path: "./.env"
-});
-connectDB();
+}); 
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 4000}`);
+    });
+})
+.catch((error) => {
+    console.log("Error connecting to MongoDB:", error); 
+})
 
 
 
