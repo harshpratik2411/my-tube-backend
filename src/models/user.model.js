1f03a03989cd65544eb9ab67854bf7dff1acb4df
harshpratik2411 <harshsrivastava2357@gmail.com>
2026-08-06 12:48:59 +0530

    user and video model created

---
+++ src/models/user.model.js
@@ -0,0 +1,91 @@
+import mongoose, { Schema } from "mongoose";
+import jsonwebtoken from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  fullName: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    index: true,
+  },
+  avatar: {
+    type: String,
+    required: true,
+  },
+  coverImage: {
+    type: String,
+    required: true,
+  },
+  watchHistory: [
+    {
+      type: Schema.Types.ObjectId,
+      ref="Video"
+    }
+  ],
+  password: {
+    type: String,
+    required: [true, "password is required"]
+  },
+  refreshtoken: {
+    type: String,
+
+  }
+
+
+},
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.comparePassword(password, this.password);
+}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model("User", userSchema);
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,15 @@
- 
- 
-
-
-
- const asyncHandler = (requestHandler) => {
-    (req,res,next) => {
-        promise.resolve(requestHandler(req,res,next)).catch((err)=> {
-            next(err)
-        })
-    }
- }
-export default asyncHandler; 
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    promise.resolve(requestHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+export default asyncHandler;
 //  const asyncHandler = (fn) => async (req, res, next) => {
 //     try {
 //        await fn(req,res,next)
-//     } 
+//     }
 //     catch (error) {
 //       res.status(err.code || 500).json({
 //         success: false,
@@ -22,4 +17,4 @@ export default asyncHandler;
 //       })
 //     }
 
- // }
+// }
